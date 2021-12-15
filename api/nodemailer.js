7155fcdd1a40ba3dce8aa33bf092855be485fefd
SenpaiXYZ <jhicer@gmail.com>
2021-12-15 18:28:16 +0800

    added something

--- api/nodemailer.js
+++ api/nodemailer.js
@@ -1,6 +1,6 @@
 const nodemailer = require("nodemailer");
 const smtpTransport = require('nodemailer-smtp-transport');
-
+require('dotenv').config();
 let transport = {
             service: "gmail",
             logger: true,
@@ -8,8 +8,8 @@ let transport = {
             auth: {
                 //user: "052d66c19eb645",
                 //pass: "7e46d40d9dbbcd"
-                user:"cerbitojocelyn0948@gmail.com",
-                pass: "Analyst!!@@###$$",
+                user: process.env.EMAIL,
+                pass: process.env.PASSWORD,
             },
         };
 let mailTransporter = nodemailer.createTransport(
